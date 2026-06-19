c7b81ca2a38d50cddf9d92c3350469096e504b73
Bittu Dhar <bdhar902@gmail.com>
2026-06-20 03:00:03 +0530

    Fix render port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-    console.log("Server is running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
